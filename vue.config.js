0c03d42d4c05d8698f009a4b38b46a7851692a6a
juno-choi <ililil9482@naver.com>
2022-04-07 16:04:09 +0900

    game vue start

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer:{
+    allowedHosts: "all",
+    proxy:{
+        "/api":{
+            target: process.env.VUE_APP_API_URL
+        }
+    }
+  }  
+})
