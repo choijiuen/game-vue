a94760070dee2fae483fbdd2ed5fcf3d6a2bd1ad
juno-choi <ililil9482@naver.com>
2022-05-05 21:35:06 +0900

    FEAT : 현재 위치 찾기

--- src/components/eats/Eats.vue
+++ src/components/eats/Eats.vue
@@ -5,12 +5,14 @@
       <div class="col-lg-6 mx-auto">
         <p class="fs-5 mb-4">정상 실행이 된다면 로그인 인증 후 정상적으로 API와 통신 중임</p>
         <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-          현재 x : {{x}} y : {{y}}
+          현재 latitude : {{latitude}} longitude : {{longitude}}
         </div>
         <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
           <button class="btn btn-secondary" @click="location">위치변경</button>
         </div>
-        
+        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mt-1">
+          <button class="btn btn-secondary" @click="locationView">위치확인</button>
+        </div>
       </div>
     </div>
   </div>
@@ -26,15 +28,30 @@ export default {
   },
   data(){
     return {
-      y : 1,
-      x : 1
+        latitude : 0,
+        longitude : 0,
     }
   },
   methods:{
     location(){
-      this.y = 2;
-      this.x = 2;
+        if(!navigator.geolocation) {
+            const msg = '위치 정보가 지원되지 않습니다.';
+            alert(msg);
+            throw msg;
+        }
+        navigator.geolocation.getCurrentPosition(position => {
+            //console.log(position.timestamp);  //위치 변경을 반환한 시간
+            this.latitude = position.coords.latitude;
+            this.longitude = position.coords.longitude;
+        });
+    },
+    locationView(){
+        window.open(`https://www.openstreetmap.org/#map=17/${this.latitude}/${this.longitude}`);
     }
+    
   }
+
 }
+
+
 </script>
