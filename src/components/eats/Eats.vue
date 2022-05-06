b2e944c6acb38eaae20455861e0ef18d371aeb66
juno-choi <ililil9482@naver.com>
2022-05-06 21:33:36 +0900

    FEAT : 현재 위치로

--- src/components/eats/Eats.vue
+++ src/components/eats/Eats.vue
@@ -5,13 +5,7 @@
       <div class="col-lg-6 mx-auto">
         <p class="fs-5 mb-4">정상 실행이 된다면 로그인 인증 후 정상적으로 API와 통신 중임</p>
         <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-          현재 latitude : {{latitude}} longitude : {{longitude}}
-        </div>
-        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-          <button class="btn btn-secondary" @click="location">위치변경</button>
-        </div>
-        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mt-1">
-          <button class="btn btn-secondary" @click="locationView">위치확인</button>
+          <button class="btn btn-secondary" @click="location">현재 위치로</button>
         </div>
       </div>
     </div>
@@ -19,27 +13,21 @@
 
   <div class="b-example-divider mb-0"></div>
   <GoogleMap api-key="AIzaSyDd4q1fnJ_2BBXJo8TgMA1-0Csgf_y6Ya8" style="width: 100%; height: 500px" :center="center" :zoom="15">
-    <Marker :options="{ position: center }" />
   </GoogleMap>
 </template>
 
 <script>
-import { GoogleMap, Marker } from "vue3-google-map";
+import { GoogleMap } from "vue3-google-map";
 
 export default {
   name: 'EatsComponent',
   props: {
     msg: String
   },
-  components: { GoogleMap, Marker},
-
-  data(){
-    const center = { lat: 0, lng: 0 };
-    return {
-        latitude : 0,
-        longitude : 0,
-        center
-    }
+  components: { GoogleMap },
+  data() {
+    const center = { lat: 37.38231400000, lng: 127.11961300000 };
+    return { center };
   },
   methods:{
     location(){
@@ -50,10 +38,10 @@ export default {
         }
         navigator.geolocation.getCurrentPosition(position => {
             //console.log(position.timestamp);  //위치 변경을 반환한 시간
-            this.latitude = position.coords.latitude;
-            this.longitude = position.coords.longitude;
-            this.center.lat = position.coords.latitude;
-            this.center.lng = position.coords.longitude;
+            this.center = {
+              lat : position.coords.latitude,
+              lng : position.coords.longitude
+            }
         });
     },
     locationView(){
@@ -62,6 +50,4 @@ export default {
     
   },
 }
-
-
 </script>
