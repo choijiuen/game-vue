2b92887db60beb0d32c8a7dd8913563a89ae5dd4
juno-choi <ililil9482@naver.com>
2022-05-07 14:41:17 +0900

    FEAT : 마크 찍기 중간 저장 및 google key 추가

--- src/components/eats/Eats.vue
+++ src/components/eats/Eats.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="bg-dark text-secondary px-4 py-5 text-center">
     <div class="py-5">
-      <h1 class="display-5 fw-bold text-white">API 연동 테스트</h1>
+      <h1 class="display-5 fw-bold text-white">메뉴를 랜덤으로 추천해줍니다</h1>
       <div class="col-lg-6 mx-auto">
-        <p class="fs-5 mb-4">정상 실행이 된다면 로그인 인증 후 정상적으로 API와 통신 중임</p>
+        <p class="fs-5 mb-4">메뉴 뽑기 시작</p>
+        
+        <p class="fs-5">1. 위치 선택</p>
         <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
           <button class="btn btn-secondary" @click="location">현재 위치로</button>
           <button class="btn btn-secondary" @click="execDaumPostcode">주소검색</button>
+        </div>
+        <p>lat : {{center.lat}}</p>
+        <p>lng : {{center.lng}}</p>
+
+        <p class="fs-5">2. 음식점 검색</p>
+        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center row">
+          <div class="col-md-9"><input class="form-control" type="text" placeholder="ex) 한식, 맥도날드, 일식, 중식, 자장면, 탕수육 ..."/></div>
+          <div class="col-md-3"><button class="btn btn-secondary" @click="makeMarkers">검색</button></div>
         </div>
       </div>
     </div>
@@ -14,11 +24,25 @@
 
   <div class="b-example-divider mb-0"></div>
   <GoogleMap api-key="AIzaSyDd4q1fnJ_2BBXJo8TgMA1-0Csgf_y6Ya8" style="width: 100%; height: 500px" :center="center" :zoom="15">
+    <div :key="index" v-for="(m, index) in markers">
+      <Marker :options="m">
+        <InfoWindow>
+          <div>
+            <h1 class="firstHeading">{{m.name}}</h1>
+            <div>
+              <p>
+                <b>{{m.name}}</b>{{m.content}}
+              </p>
+            </div>
+          </div>
+        </InfoWindow>
+      </Marker>
+    </div>
   </GoogleMap>
 </template>
 
 <script>
-import { GoogleMap } from "vue3-google-map";
+import { GoogleMap, Marker, InfoWindow } from "vue3-google-map";
 import axios from "axios";
 
 export default {
@@ -26,12 +50,16 @@ export default {
   props: {
     msg: String
   },
-  components: { GoogleMap },
+  components: { GoogleMap, Marker, InfoWindow },
   data() {
+    const googleApiKey = process.env.VUE_APP_GOOGLE_API_KEY;
     const center = { lat: 37.382314, lng: 127.119613 };
-    return { center };
+    const markers = [
+    ]
+    return { googleApiKey, center, markers };
   },
   methods:{
+    //지도 출력
     location(){
         if(!navigator.geolocation) {
             const msg = '위치 정보가 지원되지 않습니다.';
@@ -39,14 +67,13 @@ export default {
             throw msg;
         }
         navigator.geolocation.getCurrentPosition(position => {
-            console.log(typeof position.coords.latitude);
-            console.log(typeof position.coords.longitude);
             this.center = {
               lat : position.coords.latitude,
               lng : position.coords.longitude
             }
         });
     },
+    //다음 주소 검색
     execDaumPostcode() {
       new window.daum.Postcode({
         oncomplete: (data) => {
@@ -54,6 +81,7 @@ export default {
         },
       }).open();
     },
+    //주소를 기준으로 위도, 경도 정보 가져오기
     kakaoAddress(address){
       axios.get('https://dapi.kakao.com/v2/local/search/address',{
         params:{
@@ -64,7 +92,6 @@ export default {
         }
       })
       .then(res=>{
-        console.log(res);
         const roadAddress = res.data.documents[0].address;
         this.setCenter(roadAddress.x,roadAddress.y);
       })
@@ -73,6 +100,7 @@ export default {
         console.log(err);
       });
     },
+    //지도 위치 변경
     setCenter(x, y){
       const xx = Number(x);
       const yy = Number(y);
@@ -80,6 +108,16 @@ export default {
         lat : yy,
         lng : xx
       }
+    },
+    //지도에 마크 찍어내기
+    makeMarkers(){
+      this.markers = [
+        { position: { lat: 37.382314, lng: 127.119222 }, label: "L", title: "LADY LIBERTY", name: "test1", content: "내용1"},
+        { position: { lat: 37.382314, lng: 127.119111 }, label: "B", title: "LADY LIBERTY", name: "test2", content: "내용2"},
+        { position: { lat: 37.382314, lng: 127.119333 }, label: "C", title: "LADY LIBERTY", name: "test3", content: "내용3"},
+      ];
+      console.log(this.markers);
+      console.log(this.googleApiKey);
     }
   },
 }
